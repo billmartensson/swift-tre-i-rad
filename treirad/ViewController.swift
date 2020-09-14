6218250be870bcf5a4e05b20b84e806f44a5ea7c
Bill Mårtensson <bill@magictechnology.se>
2020-09-14 13:12:36 +0200

    Initial Commit

---
+++ treirad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  treirad
+//
+//  Created by Bill Martensson on 2020-09-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
